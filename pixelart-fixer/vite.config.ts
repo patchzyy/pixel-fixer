470ccef690b5d6e2d42f353053daace44fedd298
Patchzy <64382339+patchzyy@users.noreply.github.com>
2025-08-10 15:15:41 +0200

    Update vite.config.ts

--- pixelart-fixer/vite.config.ts
+++ pixelart-fixer/vite.config.ts
@@ -4,10 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  // Set base for GitHub Pages:
-  // if deploying to https://USERNAME.github.io/REPO_NAME/
-  base: '/REPO_NAME/',
-
-  // if deploying to https://USERNAME.github.io/ (user/organization site),
-  // use base: '/' or remove it entirely.
+  // Set base for GitHub Pages or sub-path hosting
+  base: '/pixaliser/',
 })
